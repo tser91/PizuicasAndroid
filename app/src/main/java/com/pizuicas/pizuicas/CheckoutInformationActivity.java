ef2527ac855ffd7f4d602a5936e7f6f504f9eaba
Sergio Saborío Taylor <ssergio92@gmail.com>
2016-03-17 02:14:23 -0600

    new: usr: Using location services, checkout info is partially filled.

--- app/src/main/java/com/pizuicas/pizuicas/CheckoutInformationActivity.java
+++ app/src/main/java/com/pizuicas/pizuicas/CheckoutInformationActivity.java
@@ -1,9 +1,16 @@
 package com.pizuicas.pizuicas;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
@@ -18,22 +25,35 @@ import android.widget.Toast;
 
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.pizuicas.pizuicas.application.ShopifyApplication;
 import com.shopify.buy.dataprovider.BuyClient;
 import com.shopify.buy.model.Address;
 import com.shopify.buy.model.Checkout;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-public class CheckoutInformationActivity extends AppCompatActivity {
+public class CheckoutInformationActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private final static int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 161; // Magic number
     /**
      * The {@link Tracker} used to record screen views.
      */
     private Tracker mTracker;
-
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
     private String TAG = CheckoutInformationActivity.class.getName();
 
     private TextInputLayout inputLayoutName, inputLayoutLastName, inputLayoutEmail,
@@ -74,6 +94,18 @@ public class CheckoutInformationActivity extends AppCompatActivity {
             }
         });
 
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
         initializeCheckout();
 
         inputLayoutName = (TextInputLayout) findViewById(R.id.inputLayout_name);
@@ -108,6 +140,36 @@ public class CheckoutInformationActivity extends AppCompatActivity {
         // Obtain the shared Tracker instance.
         mTracker = getShopifyApplication().getDefaultTracker();
         // [END shared_tracker]
+    }
+
+    private void getInfoFromLocation(double latitude, double longitude) {
+
+        Geocoder geocoder;
+        List<android.location.Address> addresses = null;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "getInfoFromLocation: addresses size is " + addresses.size());
+
+        if (addresses != null && addresses.size() > 0) {
+            //android.location.Address tempAddress = addresses.get(0);
+            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+            String postalCode = addresses.get(0).getPostalCode();
+
+            inputAddress.setText(address);
+            inputCity.setText(city);
+            inputCountryCode.setText(country);
+            inputProvince.setText(state);
+            inputZip.setText(postalCode);
+        }
     }
 
     private void initializeCheckout() {
@@ -272,8 +334,8 @@ public class CheckoutInformationActivity extends AppCompatActivity {
 
     private boolean validateCountryCode() {
         if (inputCountryCode.getText().toString().trim().isEmpty() ) {
-                //|| !inputCountryCode.getText().toString().equals(
-                  //      getResources().getString(R.string.costa_rica_code)) ) {
+            //|| !inputCountryCode.getText().toString().equals(
+            //      getResources().getString(R.string.costa_rica_code)) ) {
             inputLayoutCountryCode.setError(getString(R.string.err_msg_country));
             requestFocus(inputCountryCode);
             return false;
@@ -305,6 +367,92 @@ public class CheckoutInformationActivity extends AppCompatActivity {
         Log.e(TAG, "Error: " + errorMessage);
         Toast.makeText(this, R.string.error, Toast.LENGTH_LONG).show();
         finish();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Location services connected.");
+
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "onConnected: looking for a new location");
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+        else {
+            Log.d(TAG, "onConnected: permission not granted yet");
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+            // MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+        }
+    }
+
+    private void handleNewLocation(Location location) {
+        Log.d(TAG, location.toString());
+
+        getInfoFromLocation(location.getLatitude(), location.getLongitude());
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended. Please reconnect.");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged: ");
+        handleNewLocation(location);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "onRequestPermissionsResult: permission was granted");
+                    // permission was granted
+                    LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+                } else {
+                    Log.d(TAG, "onRequestPermissionsResult: permission was not granted");
+                }
+                return;
+            }
+        }
     }
 
     private class PisuicasTextWatcher implements TextWatcher {
